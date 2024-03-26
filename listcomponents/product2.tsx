feca2063227b702ec766c3890de2aa24df027c18
DESKTOP-UF62SM0\WalterWhite <boemhut113@gmail.com>
2024-03-26 13:12:03 +0700

    Display firebase basic homepage

--- listcomponents/product2.tsx
+++ listcomponents/product2.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import {
   Box,
   Card,
@@ -7,30 +7,92 @@ import {
   Grid,
   Typography,
 } from "@mui/material";
+import {
+  DocumentData,
+  QueryDocumentSnapshot,
+  SnapshotOptions,
+  collection,
+  getDocs,
+  getFirestore,
+} from "firebase/firestore";
+import { initializeApp } from "firebase/app";
+class Product {
+  name: string;
+  description: string;
+  price: number;
+  offer: string;
+  technical: string;
+  image: string;
+  // classId: string
+  constructor(
+    name: string,
+    description: string,
+    price: number,
+    offer: string,
+    technical: string,
+    image: string
+  ) {
+    this.name = name;
+    this.description = description;
+    this.price = price;
+    this.offer = offer;
+    this.technical = technical;
+    this.image = image;
+  }
+}
+const productConverter = {
+  toFirestore: (product: Product) => {
+    return {
+      name: product.name,
+      description: product.description,
+      price: product.price,
+      offer: product.offer,
+      technical: product.technical,
+      image: product.image,
+    };
+  },
 
+  fromFirestore: (
+    snapshot: QueryDocumentSnapshot<DocumentData, DocumentData>,
+    options: SnapshotOptions
+  ) => {
+    const data = snapshot.data(options);
+    return new Product(
+      data.name,
+      data.description,
+      data.price,
+      data.offer,
+      data.technical,
+      data.image
+    );
+  },
+};
 const Product2 = () => {
-  const products = [
-    {
-      name: "Product 1",
-      image: "/mau.png",
-      description: "Description of product 1",
-    },
-    {
-      name: "Product 2",
-      image: "/mau.png",
-      description: "Description of product 2",
-    },
-    {
-      name: "Product 3",
-      image: "/mau.png",
-      description: "Description of product 3",
-    },
-    {
-      name: "Product 4",
-      image: "/mau.png",
-      description: "Description of product 4",
-    },
-  ];
+  const [products, setProducts] = useState<Product[]>([]);
+
+  const firebaseConfig = {
+    apiKey: "AIzaSyDxPdKcUdO25lL4YivzClfgIijIbNipTjs",
+    authDomain: "fir-demo-de07f.firebaseapp.com",
+    projectId: "fir-demo-de07f",
+    storageBucket: "fir-demo-de07f.appspot.com",
+    messagingSenderId: "998152591354",
+    appId: "1:998152591354:web:f8dcd52c0037f09c333643",
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+
+  // Initialize Cloud Firestore and get a reference to the service
+  const db = getFirestore(app);
+  const getData = async () => {
+    let productListRef = collection(db, "products").withConverter(
+      productConverter
+    );
+    let productList = await getDocs(productListRef);
+    let productListData = productList.docs.map((doc) => doc.data());
+    setProducts(productListData);
+  };
+  getData();
 
   return (
     <Box>
@@ -45,7 +107,7 @@ const Product2 = () => {
                 component="img"
                 height="140"
                 image={product.image}
-                alt={product.name}
+                alt={product.image}
               />
               <CardContent>
                 <Typography gutterBottom variant="h5" component="div">
