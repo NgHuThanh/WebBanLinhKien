912e90f3f81a331d4c2e0e36db5bc2af54f5473e
TinNguyen <tin.ntr.62cntt@ntu.edu.vn>
2024-03-27 21:21:32 +0700

    Add components

---
+++ listcomponents/product2.tsx
@@ -0,0 +1,152 @@
+import React, { useEffect, useState } from "react";
+import {
+  Box,
+  Card,
+  CardContent,
+  CardMedia,
+  Grid,
+  Link,
+  Typography,
+} from "@mui/material";
+import {
+  DocumentData,
+  QueryDocumentSnapshot,
+  SnapshotOptions,
+  collection,
+  getDocs,
+  getFirestore,
+} from "firebase/firestore";
+import { initializeApp } from "firebase/app";
+class Product {
+  id: string;
+  name: string;
+  description: string;
+  price: number;
+  offer: string;
+  technical: string;
+  image: string;
+  // classId: string
+  constructor(
+    id: string,
+    name: string,
+    description: string,
+    price: number,
+    offer: string,
+    technical: string,
+    image: string
+  ) {
+    this.id = id;
+    this.name = name;
+    this.description = description;
+    this.price = price;
+    this.offer = offer;
+    this.technical = technical;
+    this.image = image;
+  }
+}
+const productConverter = {
+  toFirestore: (product: Product) => {
+    return {
+      name: product.name,
+      description: product.description,
+      price: product.price,
+      offer: product.offer,
+      technical: product.technical,
+      image: product.image,
+    };
+  },
+
+  fromFirestore: (
+    snapshot: QueryDocumentSnapshot<DocumentData, DocumentData>,
+    options: SnapshotOptions
+  ) => {
+    const data = snapshot.data(options);
+    return new Product(
+      snapshot.id,
+      data.name,
+      data.description,
+      data.price,
+      data.offer,
+      data.technical,
+      data.image
+    );
+  },
+};
+const Product2 = () => {
+  const [products, setProducts] = useState<Product[]>([]);
+
+  const firebaseConfig = {
+    apiKey: "AIzaSyATnmpP4jkLiXKx1PvknQvW992tBDGD6IU",
+    authDomain: "caijdodb.firebaseapp.com",
+    projectId: "caijdodb",
+    storageBucket: "caijdodb.appspot.com",
+    messagingSenderId: "556827412764",
+    appId: "1:556827412764:web:c0b1c4de752f91f9a592d9",
+    measurementId: "G-3YRPCJR6LV",
+  };
+
+  // Initialize Firebase
+
+  // Initialize Cloud Firestore and get a reference to the service
+
+  useEffect(() => {
+    const firebaseConfig = {
+      apiKey: "AIzaSyDxPdKcUdO25lL4YivzClfgIijIbNipTjs",
+      authDomain: "fir-demo-de07f.firebaseapp.com",
+      projectId: "fir-demo-de07f",
+      storageBucket: "fir-demo-de07f.appspot.com",
+      messagingSenderId: "998152591354",
+      appId: "1:998152591354:web:f8dcd52c0037f09c333643",
+    };
+    const app = initializeApp(firebaseConfig);
+    const db = getFirestore(app);
+    const getData = async () => {
+      let productListRef = collection(db, "products").withConverter(
+        productConverter
+      );
+      let productList = await getDocs(productListRef);
+      let productListData = productList.docs.map((doc) => doc.data());
+      setProducts(productListData);
+    };
+    getData();
+
+    // Cleanup function (optional) to unsubscribe or perform other clean-up tasks
+    // Since this effect runs only once, cleanup is not critical here
+    // But you might need it in other useEffect scenarios
+    // return () => {};
+  }, []);
+
+  return (
+    <Box>
+      <Box>
+        <h2>Top Deals on Electronics</h2>
+      </Box>
+      <Grid container spacing={2}>
+        {products.map((product, index) => (
+          <Grid item xs={6} sm={3} key={index}>
+            <Link href={`/detail/${product.id}`} underline="none">
+              <Card sx={{ maxWidth: 300 }}>
+                <CardMedia
+                  component="img"
+                  height="140"
+                  image={product.image}
+                  alt={product.image}
+                />
+                <CardContent>
+                  <Typography gutterBottom variant="h5" component="div">
+                    {product.name}
+                  </Typography>
+                  <Typography variant="body2" color="text.secondary">
+                    {product.description}
+                  </Typography>
+                </CardContent>
+              </Card>
+            </Link>
+          </Grid>
+        ))}
+      </Grid>
+    </Box>
+  );
+};
+
+export default Product2;
--- next.config.js
+++ next.config.js
@@ -2,8 +2,8 @@
 
 const { i18n } = require("./next-i18next.config");
 const nextConfig = {
-  reactStrictMode: true,
+  reactStrictMode: true, //Dòng này nếu chạy useEffecting thì đừng có dùng nó, nó sẽ x2 lần chạy....
   i18n,
 };
 
-module.exports=nextConfig
+module.exports = nextConfig;
