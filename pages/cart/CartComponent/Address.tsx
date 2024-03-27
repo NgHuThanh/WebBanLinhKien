27a0260f3cec85930a9cc76091051fbe69a8caf0
MSI\ADMIN <huynhduy8122002@gmail.com>
2024-03-27 20:42:32 +0700

    page update

--- pages/cart/CartComponent/Address.tsx
+++ pages/cart/CartComponent/Address.tsx
@@ -1,16 +1,29 @@
-import React from 'react';
-import { Box, Button, Typography } from '@mui/material';
+import React from "react";
+import { Box, Button, Typography } from "@mui/material";
 
 type Props = {};
 
 const Address: React.FC<Props> = (props) => {
   return (
-    <Box bgcolor="white" p={3} borderRadius={5} boxShadow={1} display="flex" alignItems="center">
+    <Box
+      bgcolor="white"
+      p={3}
+      borderRadius={5}
+      boxShadow={1}
+      display="flex"
+      alignItems="center"
+    >
       <Box flexGrow={1}>
         <Typography variant="body1">Deliver to </Typography>
         <Typography variant="body1">Address</Typography>
       </Box>
-      <Button variant="contained" color="primary" style={{ borderRadius: '5px' }}>Change</Button>
+      <Button
+        variant="contained"
+        color="primary"
+        style={{ borderRadius: "5px" }}
+      >
+        Changing
+      </Button>
     </Box>
   );
 };
