02e491fe711e898761710971cef53851fe39ab15
TinNguyen <tin.ntr.62cntt@ntu.edu.vn>
2024-04-09 15:38:22 +0700

    Merge

--- pages/cart/CartComponent/Billinfo.tsx
+++ pages/cart/CartComponent/Billinfo.tsx
@@ -1,5 +1,4 @@
-import React from 'react';
-import { Grid, Typography } from '@mui/material';
+import React from "react";
 
 type Props = {
   deliveryDate: string;
@@ -8,42 +7,28 @@ type Props = {
   shippingFee: number;
 };
 
-const Billinfo: React.FC<Props> = ({ deliveryDate, totalAmount, discount, shippingFee }) => {
+const Billinfo: React.FC<Props> = ({
+  deliveryDate,
+  totalAmount,
+  discount,
+  shippingFee,
+}) => {
   const totalPrice = totalAmount - discount + shippingFee;
+  <Billinfo
+    deliveryDate="2024-03-17"
+    totalAmount={100}
+    discount={10}
+    shippingFee={5}
+  />;
 
   return (
-    <Grid container spacing={2}>
-      <Grid item xs={6}>
-        <Typography variant="body1">Delivery date:</Typography>
-      </Grid>
-      <Grid item xs={6}>
-        <Typography variant="body1">{deliveryDate}</Typography>
-      </Grid>
-      <Grid item xs={6}>
-        <Typography variant="body1">Total amount:</Typography>
-      </Grid>
-      <Grid item xs={6}>
-        <Typography variant="body1">${totalAmount}</Typography>
-      </Grid>
-      <Grid item xs={6}>
-        <Typography variant="body1">Discount:</Typography>
-      </Grid>
-      <Grid item xs={6}>
-        <Typography variant="body1">${discount}</Typography>
-      </Grid>
-      <Grid item xs={6}>
-        <Typography variant="body1">Shipping fee:</Typography>
-      </Grid>
-      <Grid item xs={6}>
-        <Typography variant="body1">${shippingFee}</Typography>
-      </Grid>
-      <Grid item xs={6}>
-        <Typography variant="body1">Total price:</Typography>
-      </Grid>
-      <Grid item xs={6}>
-        <Typography variant="body1">${totalPrice}</Typography>
-      </Grid>
-   </Grid>
+    <div>
+      <div>Delivery date: {deliveryDate}</div>
+      <div>Total amount: ${totalAmount}</div>
+      <div>Discount: ${discount}</div>
+      <div>Shipping fee: ${shippingFee}</div>
+      <div>Total price: ${totalPrice}</div>
+    </div>
   );
 };
 
