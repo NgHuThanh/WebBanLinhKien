0934c181bb796c0c536f0ca617b6101add6537e6
TinNguyen <tin.ntr.62cntt@ntu.edu.vn>
2024-04-22 16:24:40 +0700

    merge

--- pages/cart/CartComponent/Billinfo.tsx
+++ pages/cart/CartComponent/Billinfo.tsx
@@ -1,5 +1,5 @@
-import { Grid, Typography } from "@mui/material";
 import React from "react";
+import { Grid, Typography } from "@mui/material";
 
 type Props = {
   deliveryDate: string;
@@ -15,58 +15,50 @@ const Billinfo: React.FC<Props> = ({
   shippingFee,
 }) => {
   const totalPrice = totalAmount - discount + shippingFee;
-  <Billinfo
-    deliveryDate="2024-03-17"
-    totalAmount={100}
-    discount={10}
-    shippingFee={5}
-  />;
 
   return (
-    <>
-      <Grid container spacing={2}>
-        <Grid item xs={6}>
-          <Typography variant="body1">Delivery date:</Typography>
-        </Grid>
-        <Grid item xs={6}>
-          <Typography variant="body1">
-            <b>{deliveryDate}</b>
-          </Typography>
-        </Grid>
-        <Grid item xs={6}>
-          <Typography variant="body1">Total amount:</Typography>
-        </Grid>
-        <Grid item xs={6}>
-          <Typography variant="body1">
-            <b>${totalAmount}</b>
-          </Typography>
-        </Grid>
-        <Grid item xs={6}>
-          <Typography variant="body1">Discount:</Typography>
-        </Grid>
-        <Grid item xs={6}>
-          <Typography variant="body1">
-            <b>${discount}</b>
-          </Typography>
-        </Grid>
-        <Grid item xs={6}>
-          <Typography variant="body1">Shipping fee:</Typography>
-        </Grid>
-        <Grid item xs={6}>
-          <Typography variant="body1">
-            <b>${shippingFee}</b>
-          </Typography>
-        </Grid>
-        <Grid item xs={6}>
-          <Typography variant="body1">Total price:</Typography>
-        </Grid>
-        <Grid item xs={6}>
-          <Typography variant="body1">
-            <b>${totalPrice}</b>
-          </Typography>
-        </Grid>
+    <Grid container spacing={2}>
+      <Grid item xs={6}>
+        <Typography variant="body1">Delivery date:</Typography>
       </Grid>
-    </>
+      <Grid item xs={6}>
+        <Typography variant="body1">
+          <b>{deliveryDate}</b>
+        </Typography>
+      </Grid>
+      <Grid item xs={6}>
+        <Typography variant="body1">Total amount:</Typography>
+      </Grid>
+      <Grid item xs={6}>
+        <Typography variant="body1">
+          <b>${totalAmount}</b>
+        </Typography>
+      </Grid>
+      <Grid item xs={6}>
+        <Typography variant="body1">Discount:</Typography>
+      </Grid>
+      <Grid item xs={6}>
+        <Typography variant="body1">
+          <b>${discount}</b>
+        </Typography>
+      </Grid>
+      <Grid item xs={6}>
+        <Typography variant="body1">Shipping fee:</Typography>
+      </Grid>
+      <Grid item xs={6}>
+        <Typography variant="body1">
+          <b>${shippingFee}</b>
+        </Typography>
+      </Grid>
+      <Grid item xs={6}>
+        <Typography variant="body1">Total price:</Typography>
+      </Grid>
+      <Grid item xs={6}>
+        <Typography variant="body1">
+          <b>${totalPrice}</b>
+        </Typography>
+      </Grid>
+    </Grid>
   );
 };
 
