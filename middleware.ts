66e05869c66237c1d5eb20dca7a632d3bd0f00f9
DESKTOP-UF62SM0\WalterWhite <boemhut113@gmail.com>
2024-04-26 23:13:35 +0700

    Add middleware

---
+++ middleware.ts
@@ -0,0 +1,22 @@
+import { getCookie } from 'cookies-next'
+import { NextResponse } from 'next/server'
+import type { NextRequest } from 'next/server'
+
+const authPath = '/login'
+
+// This function can be marked `async` if using `await` inside
+export function middleware(request: NextRequest) {
+  const sessionId = request.cookies.get('user_id')?.value
+  console.log(sessionId)
+  console.log(request.nextUrl.pathname)
+  if (request.nextUrl.pathname.startsWith('/history') && sessionId)
+    return NextResponse.redirect(new URL('/login', request.url))
+
+  if (request.nextUrl.pathname.startsWith('/cart') && !sessionId)
+    return NextResponse.redirect(new URL('/login', request.url))
+}
+
+// See "Matching Paths" below to learn more
+export const config = {
+  matcher: ['/history', '/cart']
+}
